26bae34af40e84378d008d31c05d5ff7b4c8c0ff
Adilson Futa <adilson.futa@novadev.ao>
2024-05-09 01:25:00 +0100

    created dockerfile img

---
+++ comandos-docker.md
@@ -0,0 +1,3 @@
+
+
+docker build --pull --rm -f "Dockerfile" -t minhaapi:latest "."
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const porta = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(porta);
 }
 bootstrap();
